f0f6e1049045ac1460ccf3a4554c85741a3be82e
Khasbulat Abdullin <khas.2015@gmail.com>
2024-04-13 16:26:18 +0300

    [MRGA-12] Дополнение данных для ручки по категориям документов

--- internal/app/grpc/document.go
+++ internal/app/grpc/document.go
@@ -6,16 +6,70 @@ import (
 )
 
 func (a *Implementation) GetDocumentCategories(ctx context.Context, request *pb.GetCategoriesRequest) (*pb.GetCategoriesResponse, error) {
-	categories := []*pb.DocumentCategory{{
-		CategoryName: "Семейное право",
-		DocumentTypes: []*pb.DocumentType{{
-			Type: "Заключение брака",
-			Variants: []*pb.DocumentVariant{{
-				Key:  "marryOne",
-				Name: "Заявление на заключение брака для подачи без пары",
-			}},
-		}},
-	}}
+	categories := []*pb.DocumentCategory{
+		{
+			CategoryName: "Семейное право",
+			DocumentTypes: []*pb.DocumentType{
+				{
+					Type: "Заключение брака",
+					Variants: []*pb.DocumentVariant{{
+						Key:  "marryOne",
+						Name: "Заявление на заключение брака для подачи без пары",
+					}},
+				},
+				{
+					Type: "Расторжение брака",
+					Variants: []*pb.DocumentVariant{
+						{
+							Key:  "divorce1",
+							Name: "Исковое заявление о расторжении брака",
+						},
+						{
+							Key:  "divorce2",
+							Name: "Исковое заявление о расторжении брака с одним ребенком",
+						},
+						{
+							Key:  "divorce3",
+							Name: "Исковое заявление о расторжении брака с 2 и более детьми",
+						},
+						{
+							Key:  "divorce4",
+							Name: "Исковое заявление о расторжении брака при согласии супругов",
+						},
+						{
+							Key:  "divorce5",
+							Name: "Исковое заявление о расторжении брака, когда один супруг не согласен",
+						},
+						{
+							Key:  "divorce6",
+							Name: "Исковое заявление о расторжении брака и разделе имущества",
+						},
+						{
+							Key:  "divorce7",
+							Name: "Исковое заявление о расторжении брака и взыскании алиментов",
+						},
+						{
+							Key:  "divorce8",
+							Name: "Исковое заявление о расторжении брака и определении места жительства ребенка",
+						},
+					},
+				},
+				{
+					Type: "Взыскание алиментов",
+					Variants: []*pb.DocumentVariant{
+						{
+							Key:  "aliments1",
+							Name: "Заявление на судебный приказ о взыскании алиментов",
+						},
+						{
+							Key:  "aliments2",
+							Name: "Исковое заявление на взыскание алиментов",
+						},
+					},
+				},
+			},
+		},
+	}
 
 	return &pb.GetCategoriesResponse{Categories: categories}, nil
 }
